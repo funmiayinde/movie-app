30b8eb71c7e4f0ce3f2d89fb3739fc3a4eab0f5d
funmiayinde <funmiayinde11@gmail.com>
2023-12-06 15:55:16 +0100

    fixing building issues

--- src/pages/movies.page.tsx
+++ src/pages/movies.page.tsx
@@ -1,12 +1,12 @@
 import { FC } from 'react';
-import Movies from '../components/Movies';
+// import Movies from '../components/Movies';
 import AppLayout from '../layout/app-layout';
 
 const MoviesPage: FC = () => {
   return (
     <>
       <AppLayout />
-      <Movies />
+      {/* <Movies /> */}
     </>
   );
 };
